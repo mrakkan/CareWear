16b85f56738324dbbbcdcbb180bec897f61188ef
cj <145112109+66070248cj@users.noreply.github.com>
2024-10-29 22:45:42 +0700

    Update display_bpm.ino

--- display_bpm/display_bpm.ino
+++ display_bpm/display_bpm.ino
@@ -11,9 +11,45 @@
 #define OLED_RESET -1
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+
+
+///////////////////////////// pulseSensor
+// เข้า analog pin 0
+// from PulseSensor Playground Getting_BPM_to_Monitor
+// ยังไม่ได้เอาโค้ดขึ้นจอ
+
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+
+//  Variables
+const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED = LED_BUILTIN;          // The on-board Arduino LED, close to PIN 13.
+int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
+                               // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+                               // Otherwise leave the default "550" value. 
+                               
+PulseSensorPlayground pulseSensor;  // Creates an instance of the Pulse
+// SensorPlayground object called "pulseSensor"
+
+
+///////////////////////////////////////////
+
 int var = 0;  // ตัวแปรสำหรับทดสอบแสดงผล
 void setup() {
+
+// pulse setup
+  Serial.begin(115200);
+
+  pulseSensor.analogInput(PulseWire);   
+  pulseSensor.blinkOnPulse(LED);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);   
+
+   if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.  
+   }
+
+// led setup
   Serial.begin(9600);
+
   if (!display.begin(i2c_Address, true)) {  // สั่งให้จอ OLED เริ่มทำงานที่ Address 0x3C
     Serial.println("SSD1306 allocation failed");
   } else {
@@ -22,31 +58,54 @@ void setup() {
 }
 
 void loop() {
-  display.clearDisplay();              // ลบภาพในหน้าจอทั้งหมด
+// led loop
+    display.clearDisplay();              // ลบภาพในหน้าจอทั้งหมด
 
-  display.setCursor(0, 0);
-  display.setTextSize(1.5);
-  display.setTextColor(SH110X_BLACK, SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
-  display.print("  CareWear  ");
+// pulse loop
+  // if (pulseSensor.sawStartOfBeat()) {
+  //   int var = pulseSensor.getBeatsPerMinute(); 
+    // Serial.println("♥  A HeartBeat Happened ! ");
+    // Serial.print("BPM: ");
+    // Serial.println(myBPM);
+    pulseSensor.sawStartOfBeat();
+    int var = pulseSensor.getBeatsPerMinute();
+    var = var / 2;
 
-  display.setCursor(0, 20); 
-  display.setTextSize(2.5);
-  display.setTextColor(SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
-  display.print(var, DEC);
+// led loop
+    // display.clearDisplay();              // ลบภาพในหน้าจอทั้งหมด
 
-  display.setCursor(30, 20); 
-  display.print(" BPM");
+    display.setCursor(0, 0);
+    display.setTextSize(1);
+    display.setTextColor(SH110X_BLACK, SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
+    display.print("  CareWear  ");
 
-  display.setCursor(0, 40);
-  // display.println();
-  display.setTextSize(2.5);
-  display.setTextColor(SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
-  display.print(var, DEC); //send step from gyro
-  display.setCursor(30, 40); 
-  display.print(" Step(s)");
+    display.setCursor(0, 20); 
+    display.setTextSize(1);
+    display.setTextColor(SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
+    display.print(var, DEC);
 
+    display.setCursor(30, 20); 
+    display.print(" BPM");
+
+    display.setCursor(0, 40);
+    display.setTextSize(1.5);
+    display.setTextColor(SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
+    // display.print(var, DEC); //send step from gyro
+    display.print(var);
+
+
+    display.setCursor(30, 40); 
+    display.print(" Step(s)");
+  // }
+
+
+    // delay(20);
+
+
+  delay(500);
 
   display.display();  // สั่งให้จอแสดงผล
-  var++;
-  delay(500);
+  // var++;
 }
+
+
