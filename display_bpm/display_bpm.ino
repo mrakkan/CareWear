de4b0ab6863da7f7001e6e3bb95a15462346b256
nuannim <141387437+nuannim@users.noreply.github.com>
2024-10-30 01:48:09 +0700

    update : display bpm finish

--- display_bpm/display_bpm.ino
+++ display_bpm/display_bpm.ino
@@ -1,110 +1,135 @@
+//^ /////////////////////////// oled
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 
-#define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
-//#define i2c_Address 0x3d //initialize with the I2C addr 0x3D Typically Adafruit OLED's
+#define i2c_Address 0x3c
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 #define OLED_RESET -1
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 
+//^ /////////////////////////// pulseSensor
+//* เข้า analog pin 0
+//* from PulseSensor Playground Getting_BPM_to_Monitor
+//* ยังไม่ได้เอาโค้ดขึ้นจอ
 
-///////////////////////////// pulseSensor
-// เข้า analog pin 0
-// from PulseSensor Playground Getting_BPM_to_Monitor
-// ยังไม่ได้เอาโค้ดขึ้นจอ
-
-#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+#include <PulseSensorPlayground.h>
 
 //  Variables
 const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
 const int LED = LED_BUILTIN;          // The on-board Arduino LED, close to PIN 13.
-int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
-                               // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
-                               // Otherwise leave the default "550" value. 
-                               
-PulseSensorPlayground pulseSensor;  // Creates an instance of the Pulse
-// SensorPlayground object called "pulseSensor"
+int Threshold = 550;
 
+PulseSensorPlayground pulseSensor;
 
-///////////////////////////////////////////
+int bpm_val = 0;  // ตัวแปรสำหรับทดสอบแสดงผล
 
-int var = 0;  // ตัวแปรสำหรับทดสอบแสดงผล
+//^ /////////////////////////// gy521
+const int MPU_ADDR = 0x68;
+
+int16_t accelerometer_x, accelerometer_y, accelerometer_z;
+int16_t gyro_x, gyro_y, gyro_z;
+
+char tmp_str[7];
+
+int steps = 0;
+
+char* convert_int16_to_str(int16_t i) {
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
+//^ /////////////////////////////////////////
+//^ /////////////////////////////////////////
+
 void setup() {
 
 // pulse setup
   Serial.begin(115200);
 
-  pulseSensor.analogInput(PulseWire);   
-  pulseSensor.blinkOnPulse(LED);       //auto-magically blink Arduino's LED with heartbeat.
-  pulseSensor.setThreshold(Threshold);   
+  pulseSensor.analogInput(PulseWire);
+  pulseSensor.blinkOnPulse(LED);
+  pulseSensor.setThreshold(Threshold);
 
    if (pulseSensor.begin()) {
-    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.  
+    Serial.println("We created a pulseSensor Object !");
    }
 
 // led setup
-  Serial.begin(9600);
-
+  // Serial.begin(9600);
   if (!display.begin(i2c_Address, true)) {  // สั่งให้จอ OLED เริ่มทำงานที่ Address 0x3C
     Serial.println("SSD1306 allocation failed");
   } else {
     Serial.println("OLED Start Working !!!");
   }
+
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
 }
 
 void loop() {
 // led loop
-    display.clearDisplay();              // ลบภาพในหน้าจอทั้งหมด
+  display.clearDisplay();              // ลบภาพในหน้าจอทั้งหมด
+
+// gy521
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 7*2, true);
+
+  accelerometer_x = Wire.read()<<8 | Wire.read();
+  accelerometer_y = Wire.read()<<8 | Wire.read();
+  accelerometer_z = Wire.read()<<8 | Wire.read();
+
+  gyro_x = Wire.read()<<8 | Wire.read();
+  gyro_y = Wire.read()<<8 | Wire.read();
+  gyro_z = Wire.read()<<8 | Wire.read();
+
+  if (gyro_x > 1000 || gyro_y > 1000 || gyro_z > 1000) {
+    steps++;
+  }
 
 // pulse loop
-  // if (pulseSensor.sawStartOfBeat()) {
-  //   int var = pulseSensor.getBeatsPerMinute(); 
-    // Serial.println("♥  A HeartBeat Happened ! ");
-    // Serial.print("BPM: ");
-    // Serial.println(myBPM);
-    pulseSensor.sawStartOfBeat();
-    int var = pulseSensor.getBeatsPerMinute();
-    var = var / 2;
+  pulseSensor.sawStartOfBeat();
+  int bpm_val = pulseSensor.getBeatsPerMinute();
+  bpm_val = bpm_val / 2;
 
-// led loop
-    // display.clearDisplay();              // ลบภาพในหน้าจอทั้งหมด
+// led loop (continue)
+  display.setCursor(0, 0);
+  display.setTextSize(1);
+  display.setTextColor(SH110X_BLACK, SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
+  display.print("  CareWear  ");
 
-    display.setCursor(0, 0);
-    display.setTextSize(1);
-    display.setTextColor(SH110X_BLACK, SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
-    display.print("  CareWear  ");
+  display.setCursor(0, 20); 
+  display.setTextSize(1);
+  display.setTextColor(SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
+  display.print(bpm_val, DEC);
 
-    display.setCursor(0, 20); 
-    display.setTextSize(1);
-    display.setTextColor(SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
-    display.print(var, DEC);
+  display.setCursor(30, 20); 
+  display.print(" BPM");
 
-    display.setCursor(30, 20); 
-    display.print(" BPM");
-
-    display.setCursor(0, 40);
-    display.setTextSize(1.5);
-    display.setTextColor(SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
-    // display.print(var, DEC); //send step from gyro
-    display.print(var);
+  display.setCursor(0, 40);
+  display.setTextSize(1);
+  display.setTextColor(SH110X_WHITE);  //กำหนดข้อความสีดำ ฉากหลังสีขาว
+  // display.print(bpm_val, DEC); //send step from gyro
+  display.print(steps);
 
 
-    display.setCursor(30, 40); 
-    display.print(" Step(s)");
-  // }
-
-    // delay(20);
+  display.setCursor(30, 40); 
+  display.print(" Step(s)");
 
 
   delay(500);
 
   display.display();  // สั่งให้จอแสดงผล
-  // var++;
 }
 
 
